6d9d286b12fa719885a4649ac27b3f6d8f1b7401
NickXitco <nxitco1@jhu.edu>
2020-06-28 14:27:40 -0400

    Namespaced Debug

--- public/javascripts/Debug.js
+++ public/javascripts/Debug.js
@@ -1,15 +1,14 @@
-class Debug {
-
-    static drawCrosshairs() {
+const Debug = {
+    drawCrosshairs: function () {
         push();
         strokeWeight(3);
         stroke("white");
         line(0, 10, 0, -10);
         line(-10, 0, 10, 0);
         pop();
-    }
+    },
 
-    static printMouseCoordinates() {
+    printMouseCoordinates: function () {
         let mP = MouseEvents.getVirtualMouseCoordinates();
         push();
         translate(0, 0);
@@ -19,27 +18,27 @@ class Debug {
         text(mP.x, 10, 25);
         text(mP.y, 10, 50);
         pop();
-    }
+    },
 
-    static drawScreenCrosshairs() {
+    drawScreenCrosshairs: function () {
         push();
         strokeWeight(1);
         stroke("aqua");
         line(width / 2, 0, width / 2, height);
         line(0, height / 2, width, height / 2);
         pop();
-    }
+    },
 
-    static printFPS() {
+    printFPS: function () {
         push();
         let fps = frameRate();
         fill(255);
         stroke(0);
         text("FPS: " + fps.toFixed(2), 10, height - 10);
         pop();
-    }
+    },
 
-    static debugCamera() {
+    debugCamera: function () {
         push();
         translate(0, 0);
         scale(1);
@@ -50,9 +49,9 @@ class Debug {
         text("Camera Height: " + camera.height, 10, 125);
         text("Camera Zoom: " + camera.zoom, 10, 150);
         pop();
-    }
+    },
 
-    static debugHovered() {
+    debugHovered: function () {
         push();
         translate(0, 0);
         scale(1);
@@ -61,9 +60,9 @@ class Debug {
         let name = (hoveredArtist !== null) ? hoveredArtist.name : "None";
         text("Hovered Artist: " + name, 10, 175);
         pop();
-    }
+    },
 
-    static canvasSize() {
+    canvasSize: function () {
         push();
         translate(0, 0);
         scale(1);
@@ -72,9 +71,9 @@ class Debug {
         text("Canvas Width: " + width, 10, 200);
         text("Canvas Height: " + height, 10, 225);
         pop();
-    }
+    },
 
-    static loadingStats() {
+    loadingStats: function () {
         push();
         fill("white");
         noStroke();
@@ -82,10 +81,10 @@ class Debug {
         text("Loading Quads " + loadingQuads.size, 10, 275);
         text("Unprocessed Requests " + unprocessedResponses.length, 10, 300);
         pop();
-    }
+    },
 
-    static averageTimingEvents = {};
-    static timingGraph(timingEvents) {
+    averageTimingEvents: {},
+    timingGraph: function (timingEvents) {
         push();
         rectMode(CORNER);
         fill('white');
@@ -111,20 +110,20 @@ class Debug {
             currentHeight -= 20;
         }
         pop();
-    }
+    },
 
-    static debugAll(camera, timingEvents) {
+    debugAll: function (camera, timingEvents) {
         push();
         camera.setView();
         //Debug.drawCrosshairs();
         pop();
-        Debug.debugCamera();
+        this.debugCamera();
         //Debug.drawScreenCrosshairs();
-        Debug.debugHovered();
-        Debug.canvasSize();
-        Debug.printFPS();
-        Debug.printMouseCoordinates();
-        Debug.loadingStats();
-        Debug.timingGraph(timingEvents)
+        this.debugHovered();
+        this.canvasSize();
+        this.printFPS();
+        this.printMouseCoordinates();
+        this.loadingStats();
+        this.timingGraph(timingEvents)
     }
 }
