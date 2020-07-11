2e4b0ed72662874ed9359c6bed4ed00950f17714
NickXitco <nxitco1@jhu.edu>
2020-07-10 23:03:03 -0400

    Updated backend finder

--- backend/GenreFinder.js
+++ backend/GenreFinder.js
@@ -2,7 +2,20 @@ const mongoose = require('mongoose');
 
 async function findGenre(name) {
     const Artist = mongoose.model('Artist');
-    return await Artist.find({genres: name}).exec();
+    const Genre = mongoose.model('Genre');
+    const artistPromise = Artist.find({genres: name}).exec();
+    const genrePromise = Genre.findOne({name: name}).exec();
+    return Promise.all([artistPromise, genrePromise]).then((values) => {
+        const artists = values[0];
+        const genreInfo = values[1];
+        return {
+            name: genreInfo.name,
+            artists: artists,
+            r: genreInfo.r,
+            g: genreInfo.g,
+            b: genreInfo.b
+        };
+    });
 }
 
 module.exports = {findGenre};
