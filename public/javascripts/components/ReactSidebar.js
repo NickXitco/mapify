60c64462ebf6177b3e0881ef1803dd6b1deb16a7
NickXitco <nxitco1@jhu.edu>
2020-07-11 19:58:12 -0400

    Did a bunch of crazy react stuff, gonna take a while to resolve this

--- public/javascripts/components/ReactSidebar.js
+++ public/javascripts/components/ReactSidebar.js
@@ -1,5 +1,3 @@
-'use strict';
-
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -8,39 +6,192 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var LikeButton = function (_React$Component) {
-    _inherits(LikeButton, _React$Component);
+var Clock = function (_React$Component) {
+    _inherits(Clock, _React$Component);
 
-    function LikeButton(props) {
-        _classCallCheck(this, LikeButton);
+    function Clock(props) {
+        _classCallCheck(this, Clock);
 
-        var _this = _possibleConstructorReturn(this, (LikeButton.__proto__ || Object.getPrototypeOf(LikeButton)).call(this, props));
+        var _this = _possibleConstructorReturn(this, (Clock.__proto__ || Object.getPrototypeOf(Clock)).call(this, props));
 
-        _this.state = { liked: false };
+        _this.state = { date: new Date() };
         return _this;
     }
 
-    _createClass(LikeButton, [{
-        key: 'render',
-        value: function render() {
+    //Allocate and set up resources when the clock is rendered
+
+
+    _createClass(Clock, [{
+        key: "componentDidMount",
+        value: function componentDidMount() {
             var _this2 = this;
 
-            if (this.state.liked) {
-                return 'You liked this.';
-            }
+            this.timerID = setInterval(function () {
+                return _this2.tick();
+            }, 1000);
+        }
 
+        //Remove anc clear resources when the clock is removed
+
+    }, {
+        key: "componentWillUnmount",
+        value: function componentWillUnmount() {
+            clearInterval(this.timerID);
+        }
+    }, {
+        key: "tick",
+        value: function tick() {
+            this.setState({
+                date: new Date()
+            });
+        }
+    }, {
+        key: "render",
+        value: function render() {
             return React.createElement(
-                'button',
-                { onClick: function onClick() {
-                        return _this2.setState({ liked: true });
-                    } },
-                'Like'
+                "div",
+                null,
+                React.createElement(
+                    "h1",
+                    null,
+                    "Hello, world!"
+                ),
+                React.createElement(
+                    "h2",
+                    null,
+                    "It is ",
+                    this.state.date.toLocaleTimeString(),
+                    "."
+                )
+            );
+        }
+
+        /*
+            https://reactjs.org/docs/state-and-lifecycle.html
+         */
+
+    }]);
+
+    return Clock;
+}(React.Component);
+
+var ReactSidebar = function (_React$Component2) {
+    _inherits(ReactSidebar, _React$Component2);
+
+    function ReactSidebar(props) {
+        _classCallCheck(this, ReactSidebar);
+
+        return _possibleConstructorReturn(this, (ReactSidebar.__proto__ || Object.getPrototypeOf(ReactSidebar)).call(this, props));
+    }
+
+    _createClass(ReactSidebar, [{
+        key: "componentDidMount",
+        value: function componentDidMount() {}
+    }, {
+        key: "componentWillUnmount",
+        value: function componentWillUnmount() {}
+    }, {
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "div",
+                { className: "sidebar" },
+                React.createElement("div", { className: "sidebarStroke" }),
+                React.createElement(
+                    "div",
+                    { className: "nameAndPicture" },
+                    React.createElement("div", { className: "sidebarPicture" }),
+                    React.createElement(
+                        "div",
+                        { className: "name" },
+                        React.createElement("h1", { className: "sidebarArtistName" })
+                    )
+                ),
+                React.createElement(
+                    "div",
+                    { className: "followersSection" },
+                    React.createElement("p", { className: "followerCount" }),
+                    React.createElement("p", { className: "followers" }),
+                    React.createElement("p", { className: "followerRanking" })
+                ),
+                React.createElement(
+                    "div",
+                    { className: "genresSection" },
+                    React.createElement(
+                        "h2",
+                        null,
+                        "Genres"
+                    ),
+                    React.createElement("ul", { className: "genresList" })
+                ),
+                React.createElement(
+                    "div",
+                    { className: "relatedArtistsSection" },
+                    React.createElement(
+                        "h2",
+                        null,
+                        "Related Artists"
+                    ),
+                    React.createElement("ul", { className: "relatedArtistsList" })
+                )
             );
         }
     }]);
 
-    return LikeButton;
+    return ReactSidebar;
 }(React.Component);
 
-var domContainer = document.querySelector('#reactSidebar');
-ReactDOM.render(React.createElement(LikeButton, null), domContainer);
+/*
+
+So here's the plan as I see it.
+
+We have an App component that holds:
+    -Info Box
+    -Search Box
+    -Sidebar
+    -Future UI
+    -THE CANVAS :O
+        * For the record, I have only a semblance of an idea how I'm gonna pull this
+          off. I don't yet have the grasp on React to execute this correctly first
+          time so it will probably be a trial by fire.
+ */
+
+var P5Wrapper = function (_React$Component3) {
+    _inherits(P5Wrapper, _React$Component3);
+
+    function P5Wrapper(props) {
+        _classCallCheck(this, P5Wrapper);
+
+        return _possibleConstructorReturn(this, (P5Wrapper.__proto__ || Object.getPrototypeOf(P5Wrapper)).call(this, props));
+    }
+
+    _createClass(P5Wrapper, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(ReactSidebar, null);
+        }
+    }]);
+
+    return P5Wrapper;
+}(React.Component);
+
+var App = function (_React$Component4) {
+    _inherits(App, _React$Component4);
+
+    function App(props) {
+        _classCallCheck(this, App);
+
+        return _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
+    }
+
+    _createClass(App, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(ReactSidebar, null);
+        }
+    }]);
+
+    return App;
+}(React.Component);
+
+ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
--- public/javascripts/components_jsx/ReactSidebar.jsx
+++ public/javascripts/components_jsx/ReactSidebar.jsx
@@ -1,23 +1,131 @@
-'use strict';
-
-class LikeButton extends React.Component {
+class Clock extends React.Component{
     constructor(props) {
         super(props);
-        this.state = { liked: false };
+        this.state = {date: new Date()};
+    }
+
+    //Allocate and set up resources when the clock is rendered
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    //Remove anc clear resources when the clock is removed
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
     }
 
     render() {
-        if (this.state.liked) {
-            return 'You liked this.';
-        }
-
         return (
-            <button onClick={() => this.setState({ liked: true }) }>
-                Like
-            </button>
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
+
+    /*
+        https://reactjs.org/docs/state-and-lifecycle.html
+     */
+}
+
+
+
+
+class ReactSidebar extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    render() {
+        return (
+            <div className={"sidebar"}>
+                <div className={"sidebarStroke"}/>
+                <div className={"nameAndPicture"}>
+                    <div className={"sidebarPicture"}/>
+                    <div className={"name"}>
+                        <h1 className={"sidebarArtistName"}/>
+                    </div>
+                </div>
+
+                <div className={"followersSection"}>
+                    <p className={"followerCount"}/>
+                    <p className={"followers"}/>
+                    <p className={"followerRanking"}/>
+                </div>
+
+                <div className={"genresSection"}>
+                    <h2>Genres</h2>
+                    <ul className={"genresList"}/>
+                </div>
+
+                <div className={"relatedArtistsSection"}>
+                    <h2>Related Artists</h2>
+                    <ul className={"relatedArtistsList"}/>
+                </div>
+            </div>
         );
     }
 }
 
-let domContainer = document.querySelector('#reactSidebar');
-ReactDOM.render(<LikeButton />, domContainer);
+/*
+
+So here's the plan as I see it.
+
+We have an App component that holds:
+    -Info Box
+    -Search Box
+    -Sidebar
+    -Future UI
+    -THE CANVAS :O
+        * For the record, I have only a semblance of an idea how I'm gonna pull this
+          off. I don't yet have the grasp on React to execute this correctly first
+          time so it will probably be a trial by fire.
+ */
+
+
+class P5Wrapper extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <ReactSidebar/>
+        );
+    }
+}
+
+
+class App extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <ReactSidebar/>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
