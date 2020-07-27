2e1f2b00a7949dd3e4effb87a7f16a93c00e307e
NickXitco <nxitco1@jhu.edu>
2020-07-27 19:14:02 -0400

    Added changelog component

---
+++ public/javascripts/components_jsx/Changelog.jsx
@@ -0,0 +1,20 @@
+class Changelog extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="changelog">
+                <div className="changelog-inner">
+                    <h2>Version 0.5.2</h2>
+                    <ul className="changelog-list">
+                        <li className="changelog-list-item">
+                            Cameronnnnnnn
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+}
