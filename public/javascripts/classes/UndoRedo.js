3cfccba6bef065414c2b066adad6b97169ee2618
NickXitco <nxitco1@jhu.edu>
2020-08-02 00:04:49 -0400

    Fleshed out methods

--- public/javascripts/classes/UndoRedo.js
+++ public/javascripts/classes/UndoRedo.js
@@ -1,28 +1,38 @@
 class UndoRedo {
 
-    head;
     tail;
     current;
 
     constructor() {
-        this.head = null;
         this.tail = null;
         this.current = null;
     }
 
     reset() {
-        this.head = null;
         this.tail = null;
         this.current = null;
     }
 
     addState(state) {
+        let node = {
+            state: state,
+            prev: null,
+            next: null
+        }
 
+        if (this.tail !== null) {
+            this.tail.next = node;
+        }
+
+        node.prev = this.tail;
+        this.tail = node;
+        this.current = node;
+
+        //TODO add branching by checking if tail === current at some point in this logic (idk where yet)
     }
 
     canUndo() {
         return this.current !== null && this.current.prev !== null;
-
     }
 
     canRedo() {
@@ -31,14 +41,14 @@ class UndoRedo {
 
     undo() {
         if (this.canUndo()) {
-
+            this.current = this.current.prev;
         }
         return null;
     }
 
     redo() {
         if (this.canRedo()) {
-
+            this.current = this.current.next;
         }
         return null;
     }
