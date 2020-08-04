81fb2366fa8d47758842d44277b4bd05b5a8d0e6
NickXitco <nxitco1@jhu.edu>
2020-08-04 16:39:27 -0400

    Refactored SpotifyAPI into separate file

--- backend/ArtistFinder.js
+++ backend/ArtistFinder.js
@@ -1,59 +1,10 @@
 const mongoose = require('mongoose');
-const SpotifyWebApi = require('spotify-web-api-node');
-const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
-const client = new SecretManagerServiceClient();
-let spotifyApi = null;
-
-async function getAPICredentials() {
-    /*
-    const [secret] = await client.accessSecretVersion({
-        name: 'projects/785245481415/secrets/clientSecret/versions/1'
-    })
-
-    const [id] = await client.accessSecretVersion({
-        name: 'projects/785245481415/secrets/clientID/versions/1'
-    })
-
-    return {secret: secret.payload.data.toString(), id: id.payload.data.toString()};
-     */
-    return {secret: process.env.clientSecret, id: process.env.clientID};
-}
-
-getAPICredentials().then((res) => {
-    const clientID = res.id;
-    const clientSecret = res.secret;
-
-    spotifyApi = new SpotifyWebApi({
-        clientId: clientID,
-        clientSecret: clientSecret,
-    });
-
-    setAccessToken();
-})
-
-setInterval(setAccessToken, 1000 * 60 * 60);
-
-function setAccessToken() {
-    // Retrieve an access token
-    spotifyApi.clientCredentialsGrant().then(
-        function (data) {
-            console.log('The access token expires in ' + data.body['expires_in']);
-            console.log('The access token is ' + data.body['access_token']);
-
-            // Save the access token so that it's used in future calls
-            spotifyApi.setAccessToken(data.body['access_token']);
-        },
-        function (err) {
-            console.log(
-                'Something went wrong when retrieving an access token',
-                err.message
-            );
-        }
-    );
-}
+const spotifyApiHolder = require('./SpotifyAPI');
 
 async function findArtist(query, isQueryID) {
     const Artist = mongoose.model('Artist');
+    const spotifyApi = spotifyApiHolder.getAPI();
+
     let artist;
     if (isQueryID) {
         artist = await Artist.findOne({id: query}).exec();
@@ -97,6 +48,8 @@ async function findArtist(query, isQueryID) {
 }
 
 function runSearch(searchTerm, Artist, limit) {
+    const spotifyApi = spotifyApiHolder.getAPI();
+
     return spotifyApi.searchArtists(searchTerm, {limit: limit, offset: 0})
         .then(function (data) {
             let promises = []
