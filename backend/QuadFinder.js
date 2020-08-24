7e8b5d38821eb8cf1f54329872aed26abe6f1fe1
Nick Xitco <nxitco1@jhu.edu>
2020-08-24 16:30:54 -0400

    Added node population into quad finding

--- backend/QuadFinder.js
+++ backend/QuadFinder.js
@@ -3,7 +3,25 @@ const arangoDB = require('./ArangoDB');
 async function findQuad(name) {
     const db = arangoDB.getDB();
     return await db.query(
-        `FOR q in quads FILTER q.name == \"${name}\" LIMIT 1 RETURN q`
+        `
+        FOR q IN quads
+        FILTER q.name == "${name}"
+        LIMIT 1
+        LET nodes = (
+          FOR a IN artists
+            FILTER a.id IN q.nodes
+            RETURN a
+        )
+        RETURN {
+            name: q.name,
+            x: q.x,
+            y: q.y,
+            r: q.r,
+            image: q.image,
+            leaf: q.leaf,
+            nodes: nodes
+            }
+        `
     ).then(
         cursor => cursor.all()
     );
