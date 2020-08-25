d5dde2b3898fdf9632681df3a983fa4bb7866038
Nick Xitco <nxitco1@jhu.edu>
2020-08-25 15:13:19 -0400

    Removed mongo stuff

--- backend/ArtistFinder.js
+++ backend/ArtistFinder.js
@@ -1,58 +1,5 @@
-<<<<<<< HEAD
 const arangoDB = require('./ArangoDB');
 const spotifyApiHolder = require('./SpotifyAPI');
-=======
-const mongoose = require('mongoose');
-const SpotifyWebApi = require('spotify-web-api-node');
-const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
-const client = new SecretManagerServiceClient();
-let spotifyApi = null;
-
-async function getAPICredentials() {
-    const [secret] = await client.accessSecretVersion({
-        name: 'projects/785245481415/secrets/clientSecret/versions/1'
-    })
-
-    const [id] = await client.accessSecretVersion({
-        name: 'projects/785245481415/secrets/clientID/versions/1'
-    })
-
-    return {secret: secret.payload.data.toString(), id: id.payload.data.toString()};
-}
-
-getAPICredentials().then((res) => {
-    const clientID = res.id;
-    const clientSecret = res.secret;
-
-    spotifyApi = new SpotifyWebApi({
-        clientId: clientID,
-        clientSecret: clientSecret,
-    });
-
-    setAccessToken();
-})
-
-setInterval(setAccessToken, 1000 * 60 * 60);
-
-function setAccessToken() {
-    // Retrieve an access token
-    spotifyApi.clientCredentialsGrant().then(
-        function (data) {
-            console.log('The access token expires in ' + data.body['expires_in']);
-            console.log('The access token is ' + data.body['access_token']);
-
-            // Save the access token so that it's used in future calls
-            spotifyApi.setAccessToken(data.body['access_token']);
-        },
-        function (err) {
-            console.log(
-                'Something went wrong when retrieving an access token',
-                err.message
-            );
-        }
-    );
-}
->>>>>>> gcloud-deploy
 
 async function findArtist(query, isQueryID) {
     const spotifyApi = spotifyApiHolder.getAPI();
--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
-const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
-
-
-const client = new SecretManagerServiceClient();
-
-async function accessSecretVersion() {
-    const [version] = await client.accessSecretVersion({
-        name: 'projects/785245481415/secrets/mongopass/versions/latest'
-    })
-
-    return version.payload.data.toString();
-}
-
-accessSecretVersion().then(pass => {
-    const mongoDB = "mongodb+srv://NickXitco:" + pass + "@urania.yaug6.gcp.mongodb.net/mapify?retryWrites=true&w=majority";
-    mongoose.connect(mongoDB, {useNewUrlParser: true});
-    mongoose.set('useUnifiedTopology', true);
-
-    const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'MongoDB connection error: '));
-});
 
 /* GET home page. */
 router.get('/', function (req, res) {
