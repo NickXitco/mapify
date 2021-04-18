5d670a2e41498ca43f9dbb7e7eda09b932c794f5
Nick Xitco <nxitco1@jhu.edu>
2021-04-18 17:45:50 -0400

    Restored honestly kinda broken 404 handler

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+app.use('/', indexRouter);
 app.use('/quad', quadRouter);
 app.use('/artist', artistRouter);
 app.use('/artistSearch', artistSearchRouter);
@@ -30,12 +30,21 @@ app.use('/genre', genreRouter);
 app.use('/path', shortestPathRouter);
 app.use('/random', randomRouter);
 app.use('/fence', fenceRouter);
-app.use('/', indexRouter);
-app.get('/*',function(req,res) {
-  const p = path.join(__dirname, 'public')
-  res.sendFile(p + "\\index.html");
-  res.status(200);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.statusMessage;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+})
 
 app.listen(8080, () => {
   console.log('App listening on port 8080!');
